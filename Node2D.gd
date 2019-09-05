0a0f719f9378ed73e829430ba91f47ec3358a9c2
manatlongpoonga <manatteung@gmail.com>
2019-09-05 12:21:14 +0700

    add a full game

--- Node2D.gd
+++ Node2D.gd
@@ -1,21 +1,36 @@
 extends Control
 
-#var story = "Ahrr..hay what your name. %s Nice to meet you too amm... may I have your sit side?. %s"
-#var answers = ["Aow hay my name teung nice to meet you.","Yeah sure.",]
+var story = "Ahrr..hay what your name. %s Nice to meet you too amm... may I have your sit side?. %s What are you doing?. %s"
 var questions = []
-var answers 
+var answers  = []
+var question_number = 0
+
 
 func _ready():
 	#print (story % answers) 
 	questions.append("Ahrr..hay what your name?.")
 	questions.append("Nice to meet you may I have your sit side?.")	
+	questions.append("What are you doing?.")	
 	$DisplayText.text = questions[0]
 	print (questions [0])
 		
 
 func _on_TextureButton_pressed():
-	answers = $PlayerInput.text
-	print(answers)
-	#print(len(questions))
-	print(questions[1])
-	$DisplayText.text = questions[1]
+	if question_number < len(questions)-1:
+		answers.append ($PlayerInput.text)
+		print(answers)
+		#print(len(questions))
+		$DisplayText.text = questions[question_number +1]
+		question_number = question_number +1
+		
+	#if question_number == 1:
+		#answers.append ($PlayerInput.text)
+		#print(answers)
+		#$PlayerInput.text = ""
+		#$DisplayText.text = questions[2]
+		
+	elif question_number == len(questions)-1:
+		answers.append ($PlayerInput.text)
+		print(answers)
+		$DisplayText.text = story % answers
+	$PlayerInput.text = ""
